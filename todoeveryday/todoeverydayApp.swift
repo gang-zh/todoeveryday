639d436d18657784063ba239ad3b6c78f0f9dc70
Gang Zhang <anhuizg@Gmail.com>
2026-01-06 17:20:59 -0800

    Initial Commit

---
+++ todoeveryday/todoeverydayApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoeverydayApp.swift
+//  todoeveryday
+//
+//  Created by Gang Zhang on 1/6/26.
+//
+
+import SwiftUI
+
+@main
+struct todoeverydayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
